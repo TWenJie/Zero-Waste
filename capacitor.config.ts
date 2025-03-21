3ee04953e6e3988a655b8cb6b79ee3c490e86e5e
TWenJie <93764336+TWenJie@users.noreply.github.com>
2025-03-22 02:48:49 +0800

    First

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'my.usm.cs.zerowaste.speba',
+  appName: 'speba',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
